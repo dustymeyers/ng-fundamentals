0a4b29c858badd9ef807a11e417e08f99c5625b7
Dusty Meyers <dustymeyerssculpture@gmail.com>
2022-05-18 22:19:51 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,8 @@ if (environment.production) {
   enableProdMode();
 }
 
+// Angular apps grooped into modules.
+// they are always bootstrapped with our App Component
+// This file is loaded via bootstrap by the angular.json file
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
