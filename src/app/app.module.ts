5e322af71fc3231b3ac2f74a755b7f89c86e4850
Dusty Meyers <dustymeyerssculpture@gmail.com>
2022-05-21 11:27:00 -0500

    playing with input parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventsListComponent } from './events/evets-list.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { EventsListComponent } from './events/evets-list.component';
   ],
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   bootstrap: [EventsAppComponent]
 })
