b7ac5c19ac9e028353f37deb94e1d7592a0f1e75
Dusty Meyers <dustymeyerssculpture@gmail.com>
2022-07-13 21:09:15 -0500

    strange typing fix/commonmodule, provided new services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { ToastrService } from './common/toastr.service';
+import { EventService } from './events/shared/event.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { EventsListComponent } from './events/evets-list.component';
+import { EventsListComponent } from './events/events-list.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   declarations: [
     EventsAppComponent,
@@ -16,6 +20,7 @@ import { NavBarComponent } from './nav/navbar.component';
     EventThumbnailComponent,
     NavBarComponent
   ],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
