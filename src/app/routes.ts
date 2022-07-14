a6b4e38a2d50b28ecd2cea6f412e71aff5d09a87
Dusty Meyers <dustymeyerssculpture@gmail.com>
2022-07-13 23:16:09 -0500

    creating users module

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -14,6 +14,12 @@ export const appRoutes: Routes = [
   // /events/1 ... /events/foo
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
   { path: '404', component: Error404Component},
-  { path: '', redirectTo: '/events', pathMatch: 'full'}
-
+  { path: '', redirectTo: '/events', pathMatch: 'full'},
+  {
+    path: 'user',
+    // depricated in Angular 8
+    // loadChildren: './user/user.module#UserModule'
+    loadChildren: () => import('./user/user.module')
+      .then(m => m.UserModule)
+  }
 ]
